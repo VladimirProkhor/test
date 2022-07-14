a5384f5ab7d931fb3534aa7a9bd403896ec66cba
Vladimir Prokhorov <vladimirpr1994@mail.ru>
2022-07-14 22:09:22 +0300

    2nt commit

--- src/Solution.java
+++ src/Solution.java
@@ -9,5 +9,9 @@ public class Solution {
             System.out.println("I am robot");
             System.out.println("My name is John");
             System.out.println("This is happy day");
+            System.out.println("Hello");
+            System.out.println("I am robot");
+            System.out.println("My name is John");
+            System.out.println("This is happy day");
         }
 }
